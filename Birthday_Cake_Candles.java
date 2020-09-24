b6dfb85e734eb4381e24eafa2f762a82d3b6bf8d
Fatih ASA <44146915+Fatihasa@users.noreply.github.com>
2020-09-24 22:18:12 +0200

    Create Birthday_Cake_Candles.java

---
+++ Birthday_Cake_Candles.java
@@ -0,0 +1,73 @@
+/*
+
+You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+
+Sample Input 
+
+3 2 1 3
+
+Sample Output 
+
+2
+
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<Integer> candles) {
+    
+    int candlesHigth = candles.get(0);
+    int tallCandlesNumber = 0;
+
+    for(int i : candles){
+
+        if(i > candlesHigth ){tallCandlesNumber = 1; candlesHigth = i;}
+        else if(candlesHigth == i){tallCandlesNumber++;}
+        
+    }
+
+    return tallCandlesNumber;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int candlesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> candles = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
