9febda5f0e61df3e6bfecdb0e56c5c31690deaeb
Fatih ASA <44146915+Fatihasa@users.noreply.github.com>
2020-09-25 20:17:09 +0200

    Create Grading_Students.java

---
+++ Grading_Students.java
@@ -0,0 +1,100 @@
+/*
+if the grade end with 3 or 4 roll to the 5. if the grade end with 8 or 9 roll up to the 0. if the number less then 37 don't do anything. 
+
+
+Sample Input 0
+
+4
+73
+67
+38
+33
+Sample Output 0
+
+75
+67
+40
+33
+
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+
+        int roll = 3;
+        ArrayList<Integer> newList = new ArrayList<Integer>();
+        
+        for(int i : grades){
+
+            if(i > 37){
+                if( (i%10 == 3 || i%10 == 4) && (i%10 != 8 || i%10 != 9)   ){
+
+                    i = (i/10)*10+5;
+                }if( (i%10 != 3 || i%10 != 4) && (i%10 == 8 || i%10 == 9)   ){
+
+                    i = (i/10)*10+10;
+                }
+            }
+
+            System.out.println(i);
+            newList.add(i);
+            
+        }
+
+        return newList;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int gradesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> grades = IntStream.range(0, gradesCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .map(String::trim)
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.gradingStudents(grades);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
